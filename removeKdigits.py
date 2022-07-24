27c8a10e528ac19faefb931d4f0f13b47ed84a60
Rediet-Ferew <redietasnakech@gmail.com>
2022-07-24 13:52:50 +0300

    Competitive programming

---
+++ removeKdigits.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        nums = []
+        for n in num:
+            while k > 0 and nums and nums[-1] > n:
+                k -= 1
+                nums.pop()
+            nums.append(n)
+        nums = nums[: len(nums) - k]
+        result = "".join(nums)
+        if result:
+            return result
+        
+        else:
+            return "0"
+                
+        
