87628905c9cf5a71dbdece973778825e792a4188
Rediet-Ferew <redietasnakech@gmail.com>
2022-08-31 20:44:48 +0300

    Competitive programming

---
+++ vowelsOfSubstrings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countVowels(self, word: str) -> int:
+        counter = 0
+        n = len(word)
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        for i in range(n):
+            if word[i] in vowels:
+                counter += (i + 1) * (n - i)
+        return counter
+                    
+                    
+  
