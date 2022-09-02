2e730ab382c2abdbac7a33ea9d5cf0d789b0aab4
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-09-02 18:03:57 +0300

    Time: 60 ms (31.88%), Space: 13.9 MB (64.54%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        if n >= 1 and n <= 300:
+            for j in range(0, n):
+                if nums[j] == 0 or nums[j] == 1 or nums[j] == 2:
+                    minIndex = j 
+                    for i in range(j, n):
+                        if nums[i] < nums[minIndex]:
+                            minIndex = i
+                    nums[j], nums[minIndex] = nums[minIndex], nums[j]
+            print(nums)
