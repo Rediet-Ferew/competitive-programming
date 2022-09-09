961fc38098e24350b2bdc7c3f37502e76d18b48e
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-09-09 18:54:56 +0300

    Delete Hindex.py

--- Hindex.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def hIndex(self, citations: List[int]) -> int:
-        n = len(citations)
-        citations = sorted(citations)
-        for paper, cite in enumerate(citations):
-            if n - paper <= cite:
-                return n - paper
-        return 0
-        
