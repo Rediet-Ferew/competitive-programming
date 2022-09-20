c764ea692f816335f9b2cba01e447de43e654f63
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-09-20 18:01:48 +0300

    Time: 136 ms (90.44%), Space: 16.1 MB (44.98%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,5 +1,8 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # setting boundaries and cheking the boundaries
+        # it is to check the maxright and maxleft, taking the minimum of those and then 
+        # substracting the ith height and that is the amount of water that can be trapped over that i
         n = len(height)
         left, right = 0, n - 1
         maxLeft = height[left]
@@ -15,20 +18,4 @@ class Solution:
                 right -= 1
                 maxRight = max(maxRight, height[right])
                 count += maxRight - height[right]
-        # for i in range(1, n):
-        #     maxLeft.append(max(height[: i]))
-        # for i in range(0, n - 1):
-        #     maxRight.append(max(height[i + 1: ]))
-        # maxRight.append(0)
-        # for k in range(len(maxLeft)):
-        #     minimum.append(min(maxLeft[k], maxRight[k]))
-        # for k in range(n):
-        #     diffMin = (min(maxLeft[k], maxRight[k])) - height[k] 
-        #     if diffMin > 0:
-        #         count += diffMin
-        # return count
-        # for l in range(n):
-        #     diff = minimum[l] - height[l]
-        #     if  diff > 0:
-        #         count += diff
         return count
