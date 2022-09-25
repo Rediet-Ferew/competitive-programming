197a9c7bd16cef7d73111c1bcb3ebbf45560de8b
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-09-25 16:15:48 +0300

    Time: 95 ms (35.80%), Space: 16 MB (32.13%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -2,22 +2,10 @@ class Solution:
     def frequencySort(self, s: str) -> str:
         s = list(s)
         count = Counter(s)
-        # print(count)
-        # counte = {}
-        
-        # # list1 = list(counter.items())
-        # # print(list1)
-        # print(counter.items())
         list1 = sorted(count.items(), key=lambda kv:
                  (kv[1], kv[0]))
-        print(list1)
-        # for key, val in count.items():
-        #     counte[val] = key
-        # counte = OrderedDict(counte.items())
-        # print(count.items())
         empty = ""
         while list1:
-            key, val = list1[-1]
-            list1.pop()
+            key, val = list1.pop()
             empty += (key * val)
         return empty
