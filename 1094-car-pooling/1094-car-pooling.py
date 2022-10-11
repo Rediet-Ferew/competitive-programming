4e9c3fabcfba0c66242cc41f16d800703e25d54b
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-10-11 13:28:42 +0300

    Time: 95 ms (80.28%), Space: 14.4 MB (87.95%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,16 +1,28 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        trips.sort(key = lambda t: t[1])
-        heap = []
-        currentPass = 0
+        #prefix sum solution
+        passengers = [0] * 1001
         for trip in trips:
-            passengers, start, end = trip
-            while heap and heap[0][0] <= start:
-                currentPass -= heap[0][1]
-                heapq.heappop(heap)
-            currentPass += passengers
+            passenger, start, end = trip
+            passengers[start] += passenger
+            passengers[end] -= passenger
+        currentPass = 0
+        for i in range(1001):
+            currentPass += passengers[i]
             if currentPass > capacity:
                 return False
-            heapq.heappush(heap, [end, passengers])
         return True
+        # trips.sort(key = lambda t: t[1])
+        # heap = []
+        # currentPass = 0
+        # for trip in trips:
+        #     passengers, start, end = trip
+        #     while heap and heap[0][0] <= start:
+        #         currentPass -= heap[0][1]
+        #         heapq.heappop(heap)
+        #     currentPass += passengers
+        #     if currentPass > capacity:
+        #         return False
+        #     heapq.heappush(heap, [end, passengers])
+        # return True
         
