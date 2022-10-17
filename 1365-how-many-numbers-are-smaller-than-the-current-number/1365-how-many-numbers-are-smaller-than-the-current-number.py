d7c7d7e4f646439ca86abca7bcb1133a606835dc
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-10-17 14:36:56 +0300

    Time: 99 ms (83.24%), Space: 13.9 MB (86.81%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,17 +1,25 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        counter = 0
-        mapping = {}
-        count = []
         sortedNums = sorted(nums)
-        for i in range(len(sortedNums)):
-            for num in sortedNums[: i]:
-                if num < sortedNums[i]:
-                    counter += 1
-            mapping[sortedNums[i]] = counter
-            counter = 0
-        for val in nums:
-            count.append(mapping[val])
-        return count
-                
+        result = []
+        for num in nums:
+            if num in sortedNums:
+                idx = sortedNums.index(num)
+                result.append(idx)
+                # sortedNums.pop()
+        return result
+        # counter = 0
+        # mapping = {}
+        # count = []
+        # sortedNums = sorted(nums)
+        # for i in range(len(sortedNums)):
+        #     for num in sortedNums[: i]:
+        #         if num < sortedNums[i]:
+        #             counter += 1
+        #     mapping[sortedNums[i]] = counter
+        #     counter = 0
+        # for val in nums:
+        #     count.append(mapping[val])
+        # return count
+         
                 
