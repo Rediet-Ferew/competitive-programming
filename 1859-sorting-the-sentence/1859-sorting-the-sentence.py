60fcc9d518ea75bd238b440605be3df3ea720a36
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-10-17 14:10:17 +0300

    Time: 37 ms (85.32%), Space: 13.8 MB (58.67%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,10 +1,18 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        sentence = []
-        words = {}
-        sentenceArray = s.split()
-        for word in sentenceArray:
-            words[word[-1]] = word[:-1]
-        for i in sorted(words):
-            sentence.append(words[i]) 
-        return ' '.join(sentence)
+        
+        senArr = s.split()
+        sentence = [" "]*len(senArr)
+        # words = {}
+        # sentenceArray = s.split()
+        # for word in sentenceArray:
+        #     words[word[-1]] = word[:-1]
+        # print(words)
+        # for i in sorted(words):
+        #     sentence.append(words[i]) 
+        # return ' '.join(sentence)
+        for word in senArr:
+            # print(word[-1])
+            idx = int(word[-1]) - 1
+            sentence[idx] = word[:-1]
+        return " ".join(sentence)
