d035474426607fb57bba83014a214a7521c96d1b
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-10-17 15:51:59 +0300

    Time: 777 ms (98.64%), Space: 20.5 MB (39.12%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,5 +1,6 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        #heap/priority queue solution
         # minHeap = []
         # res = []
         # for point in points:
@@ -11,21 +12,14 @@ class Solution:
         #     res.append([x,y])
         #     k -= 1
         # return res
+        #######___________________________
+        #brute sorting solution
         distances = []
         for point in points:
             dis = (point[0] ** 2) + (point[1] ** 2)
             distances.append((dis, point))
-        
         sortDistance = sorted(distances, key = lambda x : x[0],reverse=False)
-        # print(hello)
-        # print(distances[hello[0]])
         res = [p[1] for p in sortDistance[: k]]
-        # i = 0
-        # while k > 0 and i < len(sortDistance):
-        #     idx = sortDistance[i][1]
-        #     res.append(points[idx])
-        #     i += 1
-        #     k -= 1
         return res
             
         
