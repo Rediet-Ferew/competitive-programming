a5f2395795a41a83c20c955df338dd49814ae892
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-10-18 14:34:43 +0300

    Time: 1962 ms (44.12%), Space: 33.6 MB (43.21%) - LeetHub

---
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumCardPickup(self, cards: List[int]) -> int:
+        # prefix = {3 : 3, 4: 4, 2:2, 7:5 }
+        prefix = {}
+        min_len = (10 ** 5) + 1
+        for i, card in enumerate(cards):
+            if card not in prefix:
+                prefix[card] = i
+            else:
+                idx = prefix[card]
+                length = (i - idx) + 1
+                min_len = min(length, min_len)
+                prefix[card] = i
+            
+        if min_len == (10 ** 5) + 1:
+            return -1
+        else:
+            return min_len
