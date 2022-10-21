81ecd33333880ec1eb5743be8cfaf5c26eb7b2fe
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-10-21 14:04:43 +0300

    Time: 227 ms (40.58%), Space: 17.1 MB (88.83%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -12,26 +12,5 @@ class Solution:
         for key, val in Chars.items():
             result.append(val)
         return result
-            # if word in Chars:
-            #     idx = Chars[word]
-            #     temp = result[idx]
-            #     print(temp)
-            #     temp.append(s)
-                
-                
-#         prefix = []
-        
-#         for i in range(len(strs)):
-#             toAppend = sorted(strs[i])
-#             prefix.append("".join(toAppend))
-#         prefix = list(set(prefix))
-#         prefixDict = {}
-#         for word in prefix:
-#             prefixDict[word] = []
-#         for words in strs:
-#             single = "".join(sorted(words))
-#             if single in prefixDict.keys():
-#                 prefixDict[single].append(words)
-#         value = [val for val in prefixDict.values()]
-#         return value
+            
             
