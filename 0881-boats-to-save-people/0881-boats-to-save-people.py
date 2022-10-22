80faa4939709e87fe3f8c0489c63b7a5e4ac7fab
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-10-22 13:12:20 +0300

    Time: 1246 ms (17.42%), Space: 21 MB (30.22%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -15,21 +15,6 @@ class Solution:
                 people_to_go += 1
                 r -= 1
         return people_to_go
-        # n = len(people)
-        # people.sort()
-        # max_people = []
-        # left = 0
-        # right = n - 1
-        # while left <= right:
-        #     weight = people[left] + people[right]
-        #     if weight <= limit:
-        #         max_people.append((people[left], people[right]))
-        #         left += 1
-        #         right -= 1
-        #     else:
-        #         max_people.append((people[right]))
-        #         right -= 1
-        # return len(max_people)
             
                 
             
