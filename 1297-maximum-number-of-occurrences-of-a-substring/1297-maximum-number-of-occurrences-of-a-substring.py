ff9925d1bd354f629377333a9c586ce3fdf46aa2
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-10-25 21:12:19 +0300

    Time: 513 ms (46.73%), Space: 16.2 MB (73.67%) - LeetHub

--- 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.py
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.py
@@ -1,14 +1,15 @@
 class Solution:
     def maxFreq(self, s: str, maxLetters: int, minSize: int, maxSize: int) -> int:
         r = 0
-        subs = []
-        while r < len(s):
-            for end in range(r + minSize - 1, min(len(s), r + maxSize)):
-                if len(set(s[r:end+1])) <= maxLetters:
-                    subs.append(s[r:end+1])
+        
+        count = {}
+        max_occ = 0
+        while r < len(s) - minSize + 1:
+            sub = s[r: r+minSize]
+            
+            if len(set(sub)) <= maxLetters:
+                count[sub] = 1 + count.get(sub, 0)
+                max_occ = max(count[sub], max_occ)
             r += 1
-        if len(subs) == 0:
-            return 0
-        counter = Counter(subs)
-        max_occ = max(counter.values())
+        
         return max_occ
