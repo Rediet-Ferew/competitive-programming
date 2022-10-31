10e7a6360b9e95e559c25297f8ee1aecaa273f2a
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-10-31 21:26:04 +0300

    Time: 11 ms (45.51%), Space: 9.6 MB (72.03%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        bool check = std::find(std::begin(nums), std::end(nums), target) != std::end(nums);
+        if(!check){
+             nums.push_back(target);
+            sort(nums.begin(), nums.end());
+        }
+        
+        int l = 0;
+        int r = nums.size() - 1;
+        while(l <= r){
+            int mid = floor((l + r) / 2);
+            if(nums[mid] > target){
+                r = mid - 1;
+            }else if(nums[mid] < target){
+                l = mid + 1;
+            }else{
+                return mid;
+            }
+        }
+       
+        return -1;
+        
+    }
+};
