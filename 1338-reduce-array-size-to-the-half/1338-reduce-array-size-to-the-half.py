66dfd4680d56976aab35f403a59716d92b512501
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-11-01 16:11:01 +0300

    Time: 1997 ms (5.56%), Space: 37.9 MB (12.76%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -1,38 +1,17 @@
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
-        freq = Counter(arr)
+        count = Counter(arr)
         n = len(arr)
-        # print(freq[7])
-        count = freq.most_common()
-        print(count)
-        setter = set()
-        result = len(arr)
+        heap = []
+        for key, val in count.items():
+            heapq.heappush(heap, (-val, key))
+        length = n
         counter = 0
-        while result > n // 2:
-            (value, frequency) = count[counter]
-            result -= frequency
+        while length > n // 2:
+            val, key = heapq.heappop(heap)
+            length -= (-1*val)
             counter += 1
         return counter
         
+        
             
-            
-    
-        
-        
-        
-        
-        
-        # counter = 1
-        # summation = 0
-        # set1 = set(arr)
-        # if len(set1) == 1:
-        #     return 1
-        # from itertools import groupby
-        # count = [len(list(group)) for key, group in groupby(sorted(arr))]
-        # for i in range(len(count) - 1):
-        #     summation += count[i] + count[i + 1]
-        #     counter += 1
-        #     if summation > len(arr)//2:
-        #         break
-        # return counter
-        
