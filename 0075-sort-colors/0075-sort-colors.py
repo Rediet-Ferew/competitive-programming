65c99e318e536c57b8ce01f7f8cda71550d113fe
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-11-01 13:16:26 +0300

    Time: 40 ms (87.16%), Space: 13.9 MB (63.89%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,22 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-#         n = len(nums)
-#         for i in range(n):
-#             if nums[i] == 0:
-#                 del nums[i]
-#                 nums.insert(0, 0)
-#             elif nums[i] == 2:
-#                 del nums[i]
-#                 nums.insert(n - 1, 2)
-                
-#         print(nums)
-        n = len(nums)
-        if n >= 1 and n <= 300:
-            for j in range(0, n):
-                minIndex = j 
-                for i in range(j, n):
-                    if nums[i] < nums[minIndex]:
-                        minIndex = i
-                nums[j], nums[minIndex] = nums[minIndex], nums[j]
-            
+        l = 0
+        r = 0
+        end = len(nums) - 1
+        while l <= end:
+            if nums[l] == 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r += 1
+            elif nums[l] == 1:
+                l += 1
+            else:
+                nums[l], nums[end] = nums[end], nums[l]
+                end -= 1
+        
