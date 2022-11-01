db89ebdc72e62bc0442ef833b88c6ec47db90113
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-11-01 14:15:11 +0300

    Time: 1448 ms (95.63%), Space: 29.9 MB (84.70%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.py
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.py
@@ -1,22 +1,21 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        import statistics
+        nums.sort()
         n = len(nums)
-        median = 0
-        arr = [0] * (len(nums))
-        nums = sorted(nums)
-        middle = statistics.median(nums)
-        # if n%2 == 0:
-        #     middle = (nums[(n//2)- 1] + nums[n//2])//2
-        # else:
-        #     middle = nums[(n//2)- 1]
-        odd = 1
+        mid = n // 2
+        median = 0.0
+        if n % 2 == 0:
+            median =(nums[mid] + nums[mid - 1]) / 2
+        else:
+            median = float(nums[mid])
         even = 0
+        odd = 1
+        res = [0] * len(nums)
         for i in range(len(nums)):
-            if nums[i] < middle:
-                arr[odd] = nums[i]
-                odd+=2
+            if float(nums[i]) < median:
+                res[odd] = nums[i]
+                odd += 2
             else:
-                arr[even] = nums[i]
-                even+=2
-        return arr
+                res[even] = nums[i]
+                even += 2
+        return res
