2da9aee22566e3535e465cec51f3ae6b313f8f82
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-11-03 21:30:21 +0300

    Time: 3848 ms (16.58%), Space: 31.8 MB (10.25%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -1,20 +1,41 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        count = {
+            -1 : 0,
+            1 : 0,
+            0 : 0
+        }
+        
         less = []
         middle = []
         greater = []
-        final = []
+        final = [0] * len(nums)
+        
         for i in range(len(nums)):
-            if nums[i] < pivot:
-                less.append(nums[i])
-            elif nums[i] > pivot:
-                greater.append(nums[i])
+            if nums[i] > pivot:
+                count[1] += 1
+            elif nums[i] < pivot:
+                count[-1] += 1
             else:
-                middle.append(nums[i])
-        for n in less:
-            final.append(n)
-        for n in middle:
-            final.append(n)
-        for n in greater:
-            final.append(n)
+                count[0] += 1
+        
+        l = 0
+        g = len(nums) - count[1]
+        m = count[-1]
+        for j in range(len(nums)):
+            if nums[j] < pivot:
+                final[l] = nums[j]
+                l += 1
+            elif nums[j] > pivot:
+                final[g] = nums[j]
+                g += 1
+            else:
+                final[m] = nums[j]
+                m += 1
+        # for n in less:
+        #     final.append(n)
+        # for n in middle:
+        #     final.append(n)
+        # for n in greater:
+        #     final.append(n)
         return final
