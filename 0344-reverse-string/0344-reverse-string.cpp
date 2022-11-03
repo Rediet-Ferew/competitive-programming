189bf9b02c00c39cbad60c19719f835e4dd1b705
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-11-03 11:20:42 +0300

    Time: 50 ms (51.02%), Space: 23.3 MB (40.39%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        // vector<char> word;
-        // for(int i = 0; i < s.length; i++){
-        //     word.pushback(s[i]);
-        // }
         int l = 0;
         int r = s.size() - 1;
         while(l < r){
