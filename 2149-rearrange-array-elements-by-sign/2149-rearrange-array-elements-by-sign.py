f27afa04b53fe24c318abbbf022ce78cfe8db729
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-11-03 19:19:48 +0300

    Time: 2378 ms (75.50%), Space: 45.3 MB (42.76%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,20 +1,13 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        """
-        [3, -2, 1, -5, 2, 4]
-                   p
-                n
-        """
-        pos = []
-        neg = []
-        final = []
+        final = [0] * len(nums)
+        even = 0
+        odd = 1
         for num in nums:
             if num > 0:
-                pos.append(num)
+                final[even] = num
+                even += 2
             else:
-                neg.append(num)
-        
-        for i in range(len(pos)):
-            final.append(pos[i])
-            final.append(neg[i])
+                final[odd] = num
+                odd += 2
         return final
