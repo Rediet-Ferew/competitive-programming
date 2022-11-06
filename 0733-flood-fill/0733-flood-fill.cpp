3f188c8b1e8710796763630c35901c735d9226d2
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-11-06 22:26:11 +0300

    Time: 13 ms (78.38%), Space: 13.9 MB (90.95%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,18 +1,20 @@
-class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        r = len(image)
-        c = len(image[0])
-        def paint(image: List[List[int]], sr: int, sc: int, color: int, oldColor : int):
-            if sr >= r or sc >= c or sr < 0 or sc < 0 or image[sr][sc] != oldColor:
-                return;
-            image[sr][sc] = color
-            paint(image, sr - 1, sc, color, oldColor)
-            paint(image, sr + 1, sc, color, oldColor)
-            paint(image, sr, sc - 1, color, oldColor)
-            paint(image, sr, sc + 1, color, oldColor)
-        
-        
-        if image[sr][sc] == color: return image
-        paint(image, sr, sc, color, image[sr][sc])
-        return image
-        
+class Solution {
+    void paint(vector<vector<int>>& image, int sr, int sc, int color, int oldColor){
+        if (sr < 0 || sc < 0 || sr >= image.size() || sc >= image[0].size() || image[sr][sc] != oldColor){
+            return;
+        }
+        image[sr][sc] = color;
+        paint(image, sr - 1, sc, color, oldColor);
+        paint(image, sr + 1, sc, color, oldColor);
+        paint(image, sr, sc - 1, color, oldColor);
+        paint(image, sr, sc + 1, color, oldColor);
+}
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if(image[sr][sc] == color){
+            return image;
+        }
+        paint(image, sr, sc, color, image[sr][sc]);
+        return image;
+    }
+};
