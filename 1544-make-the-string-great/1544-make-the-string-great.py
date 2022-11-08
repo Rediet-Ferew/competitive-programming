62c1768af1d6cfb36437f6995b3eef343e73fb4b
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-11-08 08:39:07 +0300

    Time: 78 ms (27.20%), Space: 13.7 MB (97.60%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -3,11 +3,9 @@ class Solution:
         stack = []
         for c in s:
             if stack and chr(ord(stack[-1]) - 32) == c:
-                if stack and chr(ord(stack[-1]) - 32) == c:
-                    stack.pop()
+                stack.pop()
             elif stack and chr(ord(stack[-1]) + 32) == c:
-                if stack and chr(ord(stack[-1]) + 32) == c:
-                    stack.pop()
+                stack.pop()
             else:
                 stack.append(c)
         return "".join(stack)
