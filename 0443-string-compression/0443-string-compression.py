2548b1e0afb34555c3a0c6513d1c475995262c05
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-11-11 19:19:35 +0300

    Time: 57 ms (97.44%), Space: 14.1 MB (33.86%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,17 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        idx = 0
+        l = 0
+        while l < len(chars):
+            r = l
+            while r < len(chars) and chars[l] == chars[r]:
+                r += 1
+            chars[idx] = chars[l]
+            idx += 1
+            if r - l > 1:
+                count = str(r - l)
+                for c in count:
+                    chars[idx] = c
+                    idx += 1
+            l = r
+        return idx
