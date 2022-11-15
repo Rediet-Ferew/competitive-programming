4a3978fb1cea2a74dfa735115dcd7210cf2c314c
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-11-15 16:53:09 +0300

    Time: 89 ms (84.15%), Space: 27.7 MB (17.53%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,23 +1,16 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        # s = list(s)
-        n = len(s)
-        occurence = {}
-        left = 0
-        right = 9
-        output = []
-        while right <= n - 1:
-            temp = s[left:right + 1]
-            # temp.sort()
-            # temp = "".join(temp)
-            if temp in occurence.keys():
-                occurence[temp] += 1
-            else:
-                occurence[temp] = 1
-            left += 1
-            right += 1
-        for key, val in occurence.items():
-            if val > 1:
-                output.append(key)
-        return output
-            
+        occurrence = {}
+        l = 0
+        r = 9
+        while r < len(s):
+            dna = s[l:r+1]
+            occurrence[dna] = 1 + occurrence.get(dna, 0)
+            r += 1
+            l += 1
+        res = []
+        for dna in occurrence:
+            if occurrence[dna] > 1:
+                res.append(dna)
+        return res
+        
