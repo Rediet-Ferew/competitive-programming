8fd0af844be8849d47b95f63ee43098d5fa62e4e
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-11-16 16:01:57 +0300

    Time: 51 ms (72.28%), Space: 13.9 MB (12.34%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -1,26 +1,33 @@
 import math
 class Solution:
     def isUgly(self, n: int) -> bool:
-        if n < 0:
+        if n <= 0:
             return False
-        if n == 0:
-            return False
-        primes = set()
-        num = n
-        while num % 2 == 0:
-            primes.add(2)
-            num = num/2
-        for i in range(3, int(math.sqrt(num))+1, 2):
-            while num % i== 0:
-                # print(i)
-                primes.add(i)
-                num = num / i
-        if num > 2:
-            primes.add(int(num))
-        if len(primes) > 3:
-            return False
-        else:
-            for p in primes:
-                if p not in [2,3,5]:
-                    return False
+        if n == 1:
+            return True
+        if n % 2 == 0: return self.isUgly(n//2)
+        elif n % 3 == 0: return self.isUgly(n//3)
+        elif n % 5 == 0: return self.isUgly(n//5)
+        else: return False
         return True
+        # if n <= 0:
+        #     return False
+        # primes = set()
+        # num = n
+        # while num % 2 == 0:
+        #     primes.add(2)
+        #     num = num/2
+        # for i in range(3, int(math.sqrt(num))+1, 2):
+        #     while num % i== 0:
+        #         # print(i)
+        #         primes.add(i)
+        #         num = num / i
+        # if num > 2:
+        #     primes.add(int(num))
+        # if len(primes) > 3:
+        #     return False
+        # else:
+        #     for p in primes:
+        #         if p not in [2,3,5]:
+        #             return False
+        # return True
