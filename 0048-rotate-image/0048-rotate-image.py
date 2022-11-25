1002c2731abfef121f71b3093d8663431ac631bc
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-11-25 11:15:44 +0300

    Time: 70 ms (44.24%), Space: 14 MB (29.98%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rowLen = len(matrix)
+        colLen = len(matrix[0])
+        visited = set()
+        #Tansposing
+        for r in range(rowLen):
+            for c in range(colLen):
+                if (r,c) not in visited:
+                    matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+                visited.add((r,c))
+                visited.add((c,r))
+        # print(matrix)
+        #swapping the columns
+        for i in range(rowLen):
+            l = 0
+            r = colLen - 1
+            while l < r:
+                matrix[i][l], matrix[i][r] = matrix[i][r], matrix[i][l]
+                l += 1
+                r -= 1
+        # print(matrix)
