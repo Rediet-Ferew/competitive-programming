4f4caa5f4ed8d6d3c9a61e7ac434c7c09c8a893a
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-11-25 23:00:56 +0300

    Time: 119 ms (74.58%), Space: 14.5 MB (24.26%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
@@ -1,21 +1,18 @@
 class Solution:
     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
         hashMap = {}
-        idx_set = {}
         rowLen, colLen = len(mat), len(mat[0])
         for r in range(rowLen):
             for c in range(colLen):
                 diff = r - c
                 if diff not in hashMap:
                     hashMap[diff] = [mat[r][c]]
-                    idx_set[diff] = [(r,c)]
                 else:
                     hashMap[diff].append(mat[r][c])
-                    idx_set[diff].append((r,c))
         for key, val in hashMap.items():
             val.sort()
-        for k, v in idx_set.items():
-            for i in range(len(v)):
-                r, c = v[i]
-                mat[r][c] = hashMap[k][i]
+        for r in range(rowLen):
+            for c in range(colLen):
+                mat[r][c] = hashMap[r-c][0]
+                hashMap[r-c] = hashMap[r-c][1:]
         return mat
