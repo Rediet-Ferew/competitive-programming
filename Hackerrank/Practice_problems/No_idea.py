fe95dbe6926e6a75b8498b1ad1015b90e295ef7d
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-12-19 22:46:25 +0300

    Create No_idea.py

---
+++ Hackerrank/Practice_problems/No_idea.py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n, m = input().split(' ')
+arr =input().split(" ")
+A = set(input().split(" "))
+B = set(input().split(" "))
+happiness = 0
+for i in arr:
+    if i in A:
+        happiness += 1
+    elif i in B:
+        happiness -= 1
+    else:
+        continue
+print(happiness)
