97a1459f8c17bda065f29119115c93f79062a41c
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-12-22 14:29:23 +0300

    Time: 2199 ms (79.47%), Space: 68.9 MB (42.92%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,32 +1,19 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
         players = {}
-        for w, l in matches:
+        matches.sort()
+        for i in range(len(matches)):
+            w, l = matches[i]
             if w not in players:
-                players[w] = [1, 0]
-            else:
-                players[w][0] += 1
-            if l not in players:
-                players[l] = [0, 1]
-            else:
-                players[l][1]  += 1
-        # print(players)
-        answer_1 = []
-        answer_2 = []
-        for player in players.keys():
-            if players[player][1] == 0:
-                answer_1.append(player)
-                continue
-            if players[player][1] == 1:
-                answer_2.append(player)
-                continue
-        answer_1.sort()
-        answer_2.sort()
-        answer = [answer_1, answer_2]
-        return answer
-        # for key, val in players.items():
-        #     if val[1] == 0:
-        #         answer[0].append(key)
-        #     elif val[1] == 1:
-        #         answer[1].append(key)
-        # print(answer)
+                players[w] = 0
+            players[l] = 1+ players.get(l, 0)
+        ans1, ans2 = [], []
+        for k, v in players.items():
+            if v == 0:
+                ans1.append(k)
+            if v == 1:
+                ans2.append(k)
+        return [ans1, sorted(ans2)]
+            
+        
+        
