864d41fb7de2271cd300c0f1c2ea114f93819780
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-12-23 18:58:14 +0300

    Time: 58 ms (48.69%), Space: 13.9 MB (23.04%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -4,21 +4,30 @@ class Solution:
         ans = [''] * n
         a_to_i = {}
         j_to_z = {}
-        for i, c in enumerate("abcdefghi"):
-            idx = str(i+1)
-            a_to_i[idx] = c
-        pos = 10
-        for i, c in enumerate("jklmnopqrstuvwxyz"):
-            t = f'{pos}#'
-            j_to_z[t] = c 
-            pos += 1
+        
+        #map a to i to 1 to 9
+#         for i, c in enumerate("abcdefghi"):
+#             idx = str(i+1)
+#             a_to_i[idx] = c
+        
+#         #map j to z to 10# to 26#
+#         pos = 10
+#         for i, c in enumerate("jklmnopqrstuvwxyz"):
+#             t = f'{pos}#'
+#             j_to_z[t] = c 
+#             pos += 1
+            
         l = 0
+        num = ord('a') - 1
         while l < n:
+            
             if l < n - 2 and s[l+2] == '#':
-                temp = s[l:l+3]
-                ans[l] = j_to_z[temp]
+                temp = s[l:l+2]
+                ans[l] = chr(num + int(temp))
                 l += 3
+                
             else:
-                ans[l] = a_to_i[s[l]]
+                ans[l] = chr(num + int(s[l]))
                 l += 1
+                
         return "".join(ans)
