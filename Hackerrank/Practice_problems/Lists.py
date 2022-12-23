73b8113a79942c6809427e559698ae5df749a386
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-12-24 00:29:39 +0300

    Update Lists.py

--- Hackerrank/Practice_problems/Lists.py
+++ Hackerrank/Practice_problems/Lists.py
@@ -1,11 +1,10 @@
 if __name__ == '__main__':
     N = int(input())
     list_ = []
-    #mapping the string function names to functions onto the list
-    commands  = {"insert" : list_.insert, "pop":list_.pop,                  "print":print,"remove": list_.remove, "append": list_.append,         "sort": list_.sort, "reverse": list_.reverse}
+    #maooing string name of functions with operations on the list
+    commands  = {"insert" : list_.insert, "pop":list_.pop,"print":print,"remove": list_.remove, "append": list_.append,"sort": list_.sort, "reverse": list_.reverse}
     
     for i in range(N):
-        
         com = input()
         com = com.split(" ")
         k = com[0]
@@ -21,4 +20,4 @@ if __name__ == '__main__':
         
         elif len(com) == 3:
             commands[k](int(com[1]), int(com[2]))
-        
+        # print(list_)
