99250cbfa4e536a0e35a1a21363dcdeb66320207
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-12-23 18:59:28 +0300

    Time: 40 ms (74.90%), Space: 14 MB (23.04%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -1,33 +1,19 @@
 class Solution:
     def freqAlphabets(self, s: str) -> str:
         n = len(s)
-        ans = [''] * n
-        a_to_i = {}
-        j_to_z = {}
+        ans = ""
         
-        #map a to i to 1 to 9
-#         for i, c in enumerate("abcdefghi"):
-#             idx = str(i+1)
-#             a_to_i[idx] = c
-        
-#         #map j to z to 10# to 26#
-#         pos = 10
-#         for i, c in enumerate("jklmnopqrstuvwxyz"):
-#             t = f'{pos}#'
-#             j_to_z[t] = c 
-#             pos += 1
-            
         l = 0
         num = ord('a') - 1
         while l < n:
             
             if l < n - 2 and s[l+2] == '#':
                 temp = s[l:l+2]
-                ans[l] = chr(num + int(temp))
+                ans += chr(num + int(temp))
                 l += 3
                 
             else:
-                ans[l] = chr(num + int(s[l]))
+                ans += chr(num + int(s[l]))
                 l += 1
                 
-        return "".join(ans)
+        return ans
