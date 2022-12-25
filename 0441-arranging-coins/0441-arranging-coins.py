3f004ac2390c605a0e7f0268f353802b0d4c9ea5
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-12-25 14:26:38 +0300

    Time: 300 ms (44.09%), Space: 14 MB (11.72%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,8 +1,33 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        k = 1
-        total = 0
-        while total <= n:
-            total += k
-            k += 1
-        return k - 2
+        # k = 1
+        # total = 0
+        # while total <= n:
+        #     total += k
+        #     k += 1
+        # return k - 2
+        
+        #binary search
+        left = 1
+        right = n 
+        res = 0
+        # if n == 1:
+        #     return 1
+        while left <= right:
+            middle = (left + right)//2
+            
+            gaussian_sum = (middle * (middle + 1))//2
+            
+            if gaussian_sum > n:
+                right = middle - 1
+            else:
+                if gaussian_sum == n:
+                    return middle
+                else:
+                    
+                    while middle <= right and gaussian_sum <= n:
+                        middle += 1
+                        gaussian_sum = (middle * (middle + 1))//2
+                        
+                    return middle - 1
+                
