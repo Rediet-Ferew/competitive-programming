c16c3593ddc60a35792b53ec0e2b28acf6d3207e
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-12-30 23:53:40 +0300

    Time: 537 ms (78.83%), Space: 20.5 MB (78.83%) - LeetHub

--- 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.py
+++ 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.py
@@ -1,32 +1,28 @@
 class Solution:
     def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
         even_sum = 0
-        even_idx = set()
         l = len(queries)
         ans = []
         
         for i, n in enumerate(nums):
             if n % 2 == 0:
                 even_sum += n
-                even_idx.add(i)
+                
         
         for j in range(l):
             val, idx = queries[j]
             res = nums[idx] + val
             
-            if res % 2 == 0 and idx in even_idx:
+            if res % 2 == 0 and nums[idx] % 2 == 0:
                 even_sum -= nums[idx]
                 even_sum += res
                 
-            elif res % 2 == 0 and idx not in even_idx:
+            elif res % 2 == 0 and nums[idx] % 2 != 0:
                 even_sum += res
-                even_idx.add(idx)
                 
-            elif res % 2 != 0 and idx in even_idx:
+            elif res % 2 != 0 and nums[idx] % 2 == 0:
                 even_sum -= nums[idx]
-                even_idx.remove(idx)
                 
-            
             nums[idx] = res
             ans.append(even_sum)
             
