5eab866aa70d9a59449304ed61f20b712a4ba255
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-01-16 00:09:19 +0300

    Time: 49 ms (67.36%), Space: 14.5 MB (6.57%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -2,15 +2,12 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         rowLen = len(matrix)
         colLen = len(matrix[0])
-        for r in range(rowLen):
-            left = 0
-            right = colLen - 1
-            while left <= right:
-                mid =(left+right)//2
-                if matrix[r][mid] == target:
-                    return True
-                elif matrix[r][mid] < target:
-                    left = mid + 1
-                else:
-                    right = mid - 1
+        #calculate the unique num fo this index
+        n = ((rowLen - 1) * colLen) + (colLen - 1)
+        
+        for idx in range(n + 1):
+            row = idx // colLen
+            col = idx % colLen
+            if matrix[row][col] == target:
+                return True
         return False
