ed7b88d09711c63cebcc5d4da23459d031c799a8
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-01-16 19:33:16 +0300

    Time: 40 ms (68.35%), Space: 13.9 MB (70.45%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -6,20 +6,31 @@ class Solution:
         rowLen = len(matrix)
         colLen = len(matrix[0])
         visited = set()
-        #Tansposing
+        
         for r in range(rowLen):
             for c in range(colLen):
-                if (r,c) not in visited:
-                    matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
-                visited.add((r,c))
-                visited.add((c,r))
-        # print(matrix)
-        #swapping the columns
-        for i in range(rowLen):
-            l = 0
-            r = colLen - 1
-            while l < r:
-                matrix[i][l], matrix[i][r] = matrix[i][r], matrix[i][l]
-                l += 1
-                r -= 1
+                
+                if (r, c) not in visited:
+                    
+                    r_pos = rowLen - r - 1
+                    matrix[r][c], matrix[c][r_pos] = matrix[c][r_pos], matrix[r][c]
+                    visited.add((r,c))
+                    visited.add((c,r_pos))
+                
+        # Tansposing
+        # for r in range(rowLen):
+        #     for c in range(colLen):
+        #         if (r,c) not in visited:
+        #             matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+        #         visited.add((r,c))
+        #         visited.add((c,r))
+        # # # print(matrix)
+        # # #swapping the columns
+        # for i in range(rowLen):
+        #     l = 0
+        #     r = colLen - 1
+        #     while l < r:
+        #         matrix[i][l], matrix[i][r] = matrix[i][r], matrix[i][l]
+        #         l += 1
+        #         r -= 1
         # print(matrix)
