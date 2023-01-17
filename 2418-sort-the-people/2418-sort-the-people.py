82bed77f61a96cc690da8889899ac681a530b1b0
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-01-17 18:37:18 +0300

    Time: 2297 ms (5.03%), Space: 14.3 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -4,11 +4,13 @@ class Solution:
         n = len(names)
         
         for k in range(n):
+            swap = False
             for idx in range(n - 1):
                 if heights[idx] < heights[idx + 1]:
                     heights[idx], heights[idx + 1] = heights[idx + 1], heights[idx]
                     names[idx], names[idx + 1] = names[idx + 1], names[idx]
-
-        
-
+                    swap = True
+            if not swap:
+                break
+                
         return names
