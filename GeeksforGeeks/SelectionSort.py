8eebc59ca539f3c0a87d347eb338180c713987c1
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-01-19 21:58:50 +0300

    Update SelectionSort.py

--- GeeksforGeeks/SelectionSort.py
+++ GeeksforGeeks/SelectionSort.py
@@ -1,3 +1,4 @@
+
 class Solution: 
     def select(self, arr, i):
         m = len(arr)
@@ -7,8 +8,10 @@ class Solution:
             if arr[idx] < arr[min_idx]:
                 min_idx = idx
         return min_idx
+    
     def selectionSort(self, arr,n):
         for j in range(n - 1):
             min_i = self.select(arr, j + 1)
             arr[j], arr[min_i] = arr[min_i], arr[j]
         return arr
+
