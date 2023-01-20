11ed9aa9c8027c6e2ea9de42f51ed486b523c57e
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-01-20 21:19:36 +0300

    Time: 31 ms (93.58%), Space: 14.3 MB (15.10%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,29 +1,10 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        winner = 0
-        for i in range(1, n + 1):
-            winner = (winner + k)%i
-            
-        return (winner+1)
-        # if n == 1:
-        #     return 1
+        def winnerFinder(n, k):
+            if n == 1:
+                return 0
+            return (winnerFinder(n - 1, k) + k) % n
         
-        # return ((findTheWinner(self, n - 1, k)) + k - 1)%n+1
-#         players = []
-#         while n > 0:
-#             players.append(n)
-#             n -= 1
-#         players = players[::-1]
+        winner = winnerFinder(n, k)
         
-#         initial = 0
-#         while len(players) > 1:
-#             if initial == len(players) - 1:
-#                 intial = -1
-#                 tobeRemoved = initial + (k - 1)
-#             tobeRemoved = initial + (k - 1)
-#             print(tobeRemoved)
-#             players.remove(players[tobeRemoved])
-#             initial = tobeRemoved
-            
-#         print(players)
-        
+        return winner + 1
