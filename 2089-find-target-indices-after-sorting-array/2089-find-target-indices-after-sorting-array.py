996fb37e0cd5c1d462843215c3b34ec5a53bdce4
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-01-20 21:50:16 +0300

    Time: 50 ms (77.64%), Space: 13.8 MB (60.75%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -1,5 +1,12 @@
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        """
+        [1,2,5,2,3]
+        b = 1
+        t = 2
+        [1,2,2,3,5]
+        1, 
+        """
         output = []
         nums = sorted(nums)
         for i, num in enumerate(nums):
