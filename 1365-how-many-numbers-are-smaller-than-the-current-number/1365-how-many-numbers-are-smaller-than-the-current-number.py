99b55ae81e0da4f7c372347ee39ef110580ad31c
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-01-21 22:31:31 +0300

    Time: 121 ms (66.22%), Space: 14.1 MB (8.05%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,25 +1,20 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        sortedNums = sorted(nums)
-        result = []
-        for num in nums:
-            if num in sortedNums:
-                idx = sortedNums.index(num)
-                result.append(idx)
-                # sortedNums.pop()
-        return result
-        # counter = 0
-        # mapping = {}
-        # count = []
-        # sortedNums = sorted(nums)
-        # for i in range(len(sortedNums)):
-        #     for num in sortedNums[: i]:
-        #         if num < sortedNums[i]:
-        #             counter += 1
-        #     mapping[sortedNums[i]] = counter
-        #     counter = 0
-        # for val in nums:
-        #     count.append(mapping[val])
-        # return count
-         
-                
+        n = len(nums)
+        ans = [0] * n
+        indices = {}
+        sorted_nums = sorted(nums)
+        
+        for idx, num in enumerate(sorted_nums):
+            
+            if num in indices:
+                continue
+            else:
+                indices[num] = idx
+        
+        
+        for i, m in enumerate(nums):
+            
+            ans[i] = indices[m]
+        
+        return ans
