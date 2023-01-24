c1f9b76b61a8f38efce719fda4341408484cf005
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-01-24 18:37:46 +0300

    Time: 140 ms (53.20%), Space: 14.9 MB (37.22%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,23 +1,14 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        #prefix sum solution
-        # prefix = {}
-#         for i, num in enumerate(numbers):
-#             diff = target - num
-#             if diff in prefix:
-#                 return [prefix[diff] + 1, i + 1]
-#             prefix[num] = i
-        # return
-
         n = len(numbers)
         left = 0
         right = n - 1
         while left < right:
-            total = numbers[left] + numbers[right]
-            if total > target:
+            curr_sum = numbers[left] + numbers[right]
+            
+            if curr_sum > target:
                 right -= 1
-            elif total < target:
+            elif curr_sum < target:
                 left += 1
             else:
                 return [left + 1, right + 1]
-        return 
