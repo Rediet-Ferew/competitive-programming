632e38a2e3f0574d4db0a42fd397282c5cbc819e
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-01-25 09:20:54 +0300

    Time: 38 ms (78.33%), Space: 14 MB (32.66%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,6 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        #check for the max numbers at the end of the two arrays
+        #place the max at the end
+        
         end = n + m - 1
         while m > 0 and n > 0:
             if nums1[m - 1] > nums2[n - 1]:
@@ -12,18 +15,8 @@ class Solution:
                 nums1[end] = nums2[n - 1]
                 n -= 1
             end -= 1
+        #if remaining on the right side make them fit the first num
         while n > 0:
             nums1[end] = nums2[n - 1]
             n = n - 1
             end = end - 1
-        # if nums1 and nums2:
-        #     if m == 1 and n == 1:
-        #         nums1[0] = nums2[0]
-        #     for idx in range(m, m + n):
-        #         nums1[idx] = nums2.pop()
-        # # if m == 0:
-        # #     for i in range(n):
-        # #         nums1[i] = nums2.pop()
-        # nums1.sort()
-       
-            
