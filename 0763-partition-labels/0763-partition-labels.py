a6639c899c8729dbec42e09234d0df7ac0fe6f0c
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-01-25 18:40:02 +0300

    Time: 45 ms (61.98%), Space: 13.9 MB (15.22%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,15 +1,25 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        hashmap = {}  # character mapped to its last occurence in the string
-        for i, ch in enumerate(s):
-            hashmap[ch] = i
-        end ,size = 0, 0
-        output = []
-        for i, ch in enumerate(s):
-            size += 1
-            if hashmap[ch] > end:
-                end = hashmap[ch]
-            if i == end:
-                output.append(size)
-                size = 0
-        return output
+        last_idx = {}
+        n = len(s)
+        
+        for idx, ch in enumerate(s):
+            last_idx[ch] = idx
+        
+        part_idx = 0
+        i = 0
+        partitions = []
+        count = 1
+        while i < n:
+            end = last_idx[s[i]]
+            # print(end)
+            if end > part_idx:
+                part_idx = last_idx[s[i]] 
+            
+            if i == part_idx:
+                partitions.append(count)
+                count = 0
+                
+            count += 1
+            i += 1
+        return partitions
