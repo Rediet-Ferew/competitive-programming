44e66123146b5cc80c7600121cc4a35a6abb1f59
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-02-20 17:36:11 +0300

    Time: 57 ms (46.05%), Space: 17.3 MB (91.29%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -6,14 +6,34 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        hashMap = {}
-        size = 0
-        current = head
-        while current:
-            if current not in hashMap:
-                hashMap[current] = size
-            else:
-                return current
-            current = current.next
+        slow = head
+        fast = head
+        cycle = False
+        while fast and fast.next:
+            
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                cycle = True
+                break
+        if not cycle:
+            return 
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return slow  
+        
+        
+        
+        # hashMap = set()
+        # size = 0
+        # current = head
+        # while current:
+        #     if current not in hashMap:
+        #         hashMap.add(current)
+        #     else:
+        #         return current
+        #     current = current.next
         # return -1;
             
