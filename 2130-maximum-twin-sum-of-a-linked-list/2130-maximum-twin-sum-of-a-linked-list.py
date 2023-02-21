b4cf856deec20fb34c57eb78e0c8e321f4b6640e
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-02-21 14:53:36 +0300

    Time: 952 ms (64.79%), Space: 45.1 MB (83.44%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -5,25 +5,53 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        idx = {}
-        
-        cnt = 0
-        curr = head
-        while curr:
-            curr = curr.next
-            cnt += 1
-            
-        max_sum = float('-inf')
-        curr_2 = head
-        pos = 0
-        while curr_2:
-            
-            if (cnt - pos - 1) in idx:
-                max_sum = max(max_sum , idx[cnt - pos - 1] + curr_2.val)
-            else:
-                idx[pos] = curr_2.val
+        def reverse(head):
+            curr = head
+            prev = None
+            while curr:
+                nxt = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nxt
                 
-            pos += 1
-            curr_2 = curr_2.next
+            return prev
         
+        slow = head
+        fast = head
+        while fast:
+            slow = slow.next
+            fast = fast.next.next
+    
+        rev = reverse(slow)
+        
+        first = head
+        second = rev
+        max_sum = float('-inf')
+        while first and second:
+            max_sum = max(max_sum, first.val + second.val)
+            first = first.next
+            second = second.next
         return max_sum
+        
+#         idx = {}
+        
+#         cnt = 0
+#         curr = head
+#         while curr:
+#             curr = curr.next
+#             cnt += 1
+            
+#         max_sum = float('-inf')
+#         curr_2 = head
+#         pos = 0
+#         while curr_2:
+            
+#             if (cnt - pos - 1) in idx:
+#                 max_sum = max(max_sum , idx[cnt - pos - 1] + curr_2.val)
+#             else:
+#                 idx[pos] = curr_2.val
+                
+#             pos += 1
+#             curr_2 = curr_2.next
+        
+#         return max_sum
