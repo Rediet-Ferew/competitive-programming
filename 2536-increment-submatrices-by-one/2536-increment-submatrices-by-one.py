3a3649cb1102b495d4d0a120e0d33a468e574856
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-02-27 17:43:35 +0300

    Time: 2884 ms (44.12%), Space: 35.5 MB (63.09%) - LeetHub

---
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
+        
+        mat = [[0 for _ in range(n)] for _i in range(n)]
+    
+        for i in range(len(queries)):
+            row1, col1, row2, col2 = queries[i]
+            for j in range(row1, row2 + 1):
+                mat[j][col1] += 1
+                if col2 + 1 < n:
+                    mat[j][col2 + 1] -= 1
+        for row in range(n):
+            for col in range(1, n):
+                mat[row][col] += mat[row][col - 1]
+        return mat
