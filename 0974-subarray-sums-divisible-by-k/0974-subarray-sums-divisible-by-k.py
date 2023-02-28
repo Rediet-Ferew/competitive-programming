bdf74fd684fbc1d8846c3ad1e0f5ee9c64188865
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-02-28 21:54:16 +0300

    Time: 306 ms (55.63%), Space: 19 MB (15.97%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -1,14 +1,30 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        preSum = {0:1}
-        result = 0
+        """
+        prefix = {0:1, 4:3,2:1}
+        [4,5,0,-2,-3,1]
+        
+        sum = 5
+        count = 7
+        k = 5
+        if we get the same remainder then th
+        """
+        n = len(nums)
+        mods = {0:1}
         total = 0
-        for i in range(len(nums)):
+        subarrays = 0
+        
+        for i in range(n):
             total += nums[i]
             mod = total % k
-            if mod in preSum:
-                result += preSum[mod]
-                preSum[mod] += 1
+            
+            if mod in mods:
+                
+                subarrays += mods[mod]
+                mods[mod] += 1
+                
             else:
-                preSum[mod] = 1
-        return result
+                
+                mods[mod] = 1
+                
+        return subarrays
