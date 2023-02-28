e3e895607494bdb12351b2002c69f416c9457312
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-02-28 16:23:35 +0300

    Time: 951 ms (35.86%), Space: 20.6 MB (69.94%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,35 +1,22 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         
-        n = len(nums)
-        
-        for i in range(n):
-            if nums[i] % 2 != 0:
-                nums[i] = 1
-            else:
-                nums[i] = 0
-#         odd_count = 0
-#         left = 0
-#         right = 0
-#         subarrays = 0
-#         for right in range(n):
-#             if nums[right] == 1:
-#                 odd_count += 1
-#             while odd_count == k:
-#                 subarrays += 1
-#                 if nums[left] == 1:
-#                     odd_count -= 1
-#                 left += 1
-        
-#         return subarrays
+        def atmost(nums, k):
+            left = 0
+            right = 0
+            odd_count = 0
+            subarrays = 0
+            
+            for right in range(len(nums)):
+                if nums[right] % 2 != 0:
+                    odd_count += 1
+                while odd_count > k:
+                    if nums[left] % 2 != 0:
+                        odd_count -= 1
+                    left += 1
+                subarrays += (right - left + 1)
                 
-        counter = 0
-        summation = 0
-        prefixSum = {0 : 1}
-        for i in nums:
-            summation += i
-            case = summation - k
-            if prefixSum.get(case):
-                counter += prefixSum.get(case)
-            prefixSum[summation] = prefixSum.get(summation, 0) + 1
-        return counter
+            return subarrays
+        
+        ans = atmost(nums, k) - atmost(nums, k - 1)
+        return ans
