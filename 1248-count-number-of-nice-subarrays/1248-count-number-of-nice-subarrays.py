4c8e3f575f6c41d79d9b37ea6f84ae66ef92d872
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-02-28 16:14:16 +0300

    Time: 900 ms (53.00%), Space: 18.9 MB (99.81%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,15 +1,33 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        for i in range (len(nums)):
-            if nums[i]%2 == 0:
-                nums[i] = 0
-            else:
+        
+        n = len(nums)
+        
+        for i in range(n):
+            if nums[i] % 2 != 0:
                 nums[i] = 1
+            else:
+                nums[i] = 0
+#         odd_count = 0
+#         left = 0
+#         right = 0
+#         subarrays = 0
+#         for right in range(n):
+#             if nums[right] == 1:
+#                 odd_count += 1
+#             while odd_count == k:
+#                 subarrays += 1
+#                 if nums[left] == 1:
+#                     odd_count -= 1
+#                 left += 1
+        
+#         return subarrays
+                
         counter = 0
         summation = 0
         prefixSum = {0 : 1}
-        for n in nums:
-            summation += n
+        for i in nums:
+            summation += i
             case = summation - k
             if prefixSum.get(case):
                 counter += prefixSum.get(case)
