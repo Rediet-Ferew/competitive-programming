43721b996b12030e979255a19484416156a36b10
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-03-08 12:32:21 +0300

    Time: 44 ms (16.62%), Space: 13.8 MB (99.68%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -5,35 +5,15 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        stack = [(root.left, root.right)]
-        result = True
-        while stack:
-            root1, root2 = stack.pop()
-            
-            if not root1 and not root2:
-                continue
-            if (not root1 and root2) or (not root2 and root1) or root1.val != root2.val:
-                result = False
+    def isEqual(self, root1, root2):
+        if root1 == None or root2 == None:
+            if root1 or root2:
                 return False
-            
-            stack.append((root1.left, root2.right))
-            stack.append((root1.right, root2.left))
-        return result
-#         def symmetric(l, r):
-#             if l == None and r == None:
-#                 return True
-#             if (not l and r) or (not r and l):
-#                 return False
-
-#             val1 = l.val 
-#             val2 = r.val 
-
-#             left_check = symmetric(l.left , r.right)
-#             right_check = symmetric(l.right, r.left)
-
-#             return (left_check and right_check and (val1 == val2))
-#         if not root or (not root.left and not root.right):
-#             return True
-#         else:
-#             return symmetric(root.left, root.right)
+            return True
+        if root1.val != root2.val:
+            return False
+        return self.isEqual(root1.left, root2.right) and self.isEqual(root1.right, root2.left)
+    
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.isEqual(root.right, root.left)
+        
