f0271404373f12abcae8bc875fffdf486ff4aa8a
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-07-14 22:18:02 +0300

    added solving-questions-with-brainpower.python

---
+++ solving-questions-with-brainpower.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        n = len(questions)
+        dp = [0] * n
+        
+        for i in range(n-1, -1, -1):
+            idx = i + questions[i][1] + 1
+            if idx < n:
+                dp[i] = dp[idx] + questions[i][0]
+            else:
+                dp[i] = questions[i][0]
+            
+            if i < n-1:
+                dp[i] = max(dp[i], dp[i+1])
+        
+        return dp[0]
