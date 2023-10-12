a41f3b39fd24fb4ecd2b1d86882cf714e61788de
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-10-12 21:28:11 +0300

    added count-all-valid-pickup-and-delivery-options.python

---
+++ count-all-valid-pickup-and-delivery-options.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countOrders(self, n: int) -> int:
+        
+        size = 2*n
+        mod = 10**9 + 7
+                
+        return (math.factorial(size)//2**n) % mod
