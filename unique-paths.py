bc7b747e758e0a421ad57218f77e4728cb721853
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2023-10-15 20:52:26 +0300

    added unique-paths.python

--- unique-paths.py
+++ unique-paths.py
@@ -1,22 +1,8 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # memo = {}
         def isInBound(r, c):
             return r >= 0 and r < m and c >= 0 and c < n
-        # @cache
-        # def dp(r, c):
-        #     if r == m - 1 and c == n - 1:
-        #         return 1
-        #     ways = 0
-            
-        #     if isInBound(r + 1, c):
-        #         ways += dp(r + 1, c) 
-            
-        #     if isInBound(r, c + 1):
-        #         ways += dp(r, c + 1)
-        #     return ways
-            
-        # return dp(0, 0)
+        
         dp = []
         for i in range(m):
             temp = [0] * n
@@ -30,4 +16,5 @@ class Solution:
                     dp[r][c] += dp[r - 1][c]
                 if isInBound(r, c - 1):
                     dp[r][c] += dp[r][c - 1]
+        print(dp)
         return dp[m - 1][n - 1]
